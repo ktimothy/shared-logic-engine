8914b6da6dcce2727ca65ffa5b7209085df848aa
Mikhail Gaenkov <gaen.wrk@gmail.com>
2016-01-03 17:58:36 +0300

    cleanup

--- src-client/sle/core/models/Value.hx
+++ src-client/sle/core/models/Value.hx
@@ -7,8 +7,6 @@ class Value extends ValueBase
 {
     public function new() super();
 
-    private var fgsfds:ComplexValueMapBase<Value> = new ComplexValueMapBase<Value>();
-
     override public function process(action:sle.shim.ActionDump):Void
     {
         throw new Error('This model has no field "${action.path[0]}"');
